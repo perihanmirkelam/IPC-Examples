54c0fb8f499a4cc0c917a3768e19486b47baf0a7
Perihan Mirkelam <perihan.mirkelam@ardictech.com>
2021-01-01 07:44:32 +0300

    Send broadcast with explicit intent. Show the time broadcast was sent.

--- IPCClient/app/src/main/java/com/pmirkelam/ipcclient/ui/broadcast/BroadcastFragment.kt
+++ IPCClient/app/src/main/java/com/pmirkelam/ipcclient/ui/broadcast/BroadcastFragment.kt
@@ -1,25 +1,65 @@
 package com.pmirkelam.ipcclient.ui.broadcast
 
+import android.content.ComponentName
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.pmirkelam.ipcclient.R
+import com.pmirkelam.ipcclient.DATA
+import com.pmirkelam.ipcclient.PACKAGE_NAME
+import com.pmirkelam.ipcclient.PID
+import com.pmirkelam.ipcclient.databinding.FragmentBroadcastBinding
+import android.os.Process
+import java.util.*
+import kotlin.time.ExperimentalTime
 
-class BroadcastFragment : Fragment() {
+
+class BroadcastFragment : Fragment(), View.OnClickListener {
+
+    private var _binding: FragmentBroadcastBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentBroadcastBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
 
-        val root = inflater.inflate(R.layout.fragment_broadcast, container, false)
-        val textView: TextView = root.findViewById(R.id.text_broadcast)
-        return root
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.btnConnect.setOnClickListener(this)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onClick(v: View?) {
+        sendBroadcast()
+        showBroadcastTime()
+    }
+
+    private fun sendBroadcast(){
+        val intent = Intent()
+        intent.action = "com.pmirkelam.ipcclient"
+        intent.putExtra(PACKAGE_NAME, context?.packageName)
+        intent.putExtra(PID, Process.myPid().toString())
+        intent.putExtra(DATA, binding.edtClientData.text.toString())
+        intent.component = ComponentName("com.pmirkelam.ipcserver", "com.pmirkelam.ipcserver.IPCBroadcastReceiver")
+        activity?.applicationContext?.sendBroadcast(intent)
+    }
+
+    private fun showBroadcastTime(){
+        val cal = Calendar.getInstance()
+        val time ="${cal.get(Calendar.HOUR)}:${cal.get(Calendar.MINUTE)}:${cal.get(Calendar.SECOND)}"
+        binding.linearLayoutClientInfo.visibility = View.VISIBLE
+        binding.txtDate.text = time
     }
 }
